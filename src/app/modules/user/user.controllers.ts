dc0f93f20cd91408ebc39cf764ebf23f713bfc27
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-01-30 20:54:35 +0600

    user block unblock created

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -2,9 +2,7 @@ import catchAsync from "../../utils/CatchAsync";
 import sendResponse from "../../utils/SendResponse";
 import { userServices } from "./user.services";
 
-const {  blockUserIntoDB } = userServices;
-
-
+const { blockUserIntoDB, getAllUsersFromDB,unblockUserIntoDB } = userServices;
 
 const blockUser = catchAsync(async (req, res) => {
   const blockId = req.params.userId;
@@ -18,11 +16,32 @@ const blockUser = catchAsync(async (req, res) => {
     // data: {},
   });
 });
+const unBlockUser = catchAsync(async (req, res) => {
+  const blockId = req.params.userId;
 
+  await unblockUserIntoDB(blockId);
 
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "User unblocked successfully!",
+    // data: {},
+  });
+});
+
+const getAllUsers = catchAsync(async (req, res) => {
+  const result = await getAllUsersFromDB();
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "User Retrieved successfully!",
+    data: result,
+  });
+});
 
 export const userControllers = {
   // createUser,
-  blockUser,
- 
+  getAllUsers,
+  blockUser,unBlockUser
 };
