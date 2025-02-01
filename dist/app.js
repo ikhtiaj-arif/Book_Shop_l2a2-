ad07fd1e28e78b520d9d6a329ad5d3348724a544
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-02-01 13:56:21 +0600

    broken code

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const ErrorHandler_1 = __importDefault(require("./app/middlewears/ErrorHandler"));
 const routes_1 = __importDefault(require("./app/routes"));
@@ -12,7 +12,11 @@ const app = (0, express_1.default)();
 const port = 3000;
 //parsers
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: 'http://localhost:5173', credentials: true }));
+app.use((0, cors_1.default)({
+    origin: "https://book-shop-client-l2a4.vercel.app",
+    credentials: true,
+}));
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use((0, cookie_parser_1.default)());
 //application routes
 // app.use("/api", userRoutes);
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -4,9 +4,9 @@ import { userControllers } from "./user.controllers";
 
 const router = express.Router();
 
-const { getAllUsers,blockUser,unBlockUser } = userControllers;
+const { getAllUsers, blockUser, unBlockUser } = userControllers;
 
-router.get('/', auth("admin"), getAllUsers)
+router.get("/", auth("admin"), getAllUsers);
 router.patch("/:userId/block", auth("admin"), blockUser);
 router.patch("/:userId/unblock", auth("admin"), unBlockUser);
 
